522126351bcbe70d3a70cf0ab8e3d0420a59feb6
Thomas Swann <ts.swann@gmail.com>
2016-08-20 21:27:43 +0100

    Adding jade templates

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index.jade');
+});
+
+app.get('/admin/rooms', function(req, res) {
+    res.render('rooms.jade');
 });
 
 app.listen(3000, function() {
