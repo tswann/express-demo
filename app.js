97ded5479dc59f15558a091d5d40a524caaec5cd
Thomas Swann <ts.swann@gmail.com>
2016-08-22 20:32:45 +0100

    Adding rooms data

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require("express");
+var rooms = require('./data/rooms.json');
 var app = express();
 
+app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
 app.get('/', function(req, res) {
-  res.render('index.jade');
+  res.render('index', { title: "Home"});
 });
 
 app.get('/admin/rooms', function(req, res) {
-    res.render('rooms.jade');
+    res.render('rooms', { title: "Admin Rooms", rooms: rooms });
 });
 
 app.listen(3000, function() {
