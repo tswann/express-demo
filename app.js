8d4991a1a4961ec28857d576003f9ff791c17e52
Thomas Swann <ts.swann@gmail.com>
2016-08-24 20:15:58 +0100

    Add chatroom view

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require("express");
-var rooms = require('./data/rooms.json');
-var app = express();
+var express = require('express')
+var rooms = require('./data/rooms.json')
+var app = express()
 
-app.set('view engine', 'jade');
-app.set('views', './views');
-app.use(express.static('public'));
-app.use(express.static('node_modules/bootstrap/dist'));
+app.set('view engine', 'jade')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.static('node_modules/bootstrap/dist'))
 
-app.get('/', function(req, res) {
-  res.render('index', { title: "Home"});
-});
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Home' })
+})
 
-app.get('/admin/rooms', function(req, res) {
-    res.render('rooms', { title: "Admin Rooms", rooms: rooms });
-});
+app.get('/admin/rooms', function (req, res) {
+  res.render('rooms', { title: 'Admin Rooms', rooms: rooms })
+})
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000');
-});
+app.get('/admin/rooms/add', function (req, res) {
+  res.render('add')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000')
+})
