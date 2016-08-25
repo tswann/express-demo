d4b1498e39fd79123a263e294766d6b6f01285c1
Thomas Swann <ts.swann@gmail.com>
2016-08-25 20:32:49 +0100

    Added edit action

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var rooms = require('./data/rooms.json')
 var bodyParser = require('body-parser')
 var uuid = require('node-uuid')
+var _ = require('lodash')
 var app = express()
 
 app.set('view engine', 'jade')
@@ -36,7 +37,24 @@ app.post('/admin/rooms/add', function (req, res) {
 app.get('/admin/rooms/delete/:id', function (req, res) {
   var roomId = req.params.id
 
-  rooms = rooms.filter(r => r.id !== roomId)
+  rooms = _.find(rooms, r => r.id !== roomId)
+
+  res.redirect('/admin/rooms')
+})
+
+app.get('/admin/rooms/edit/:id', function (req, res) {
+  var roomId = req.params.id
+
+  var room = _.find(rooms, r => r.id === roomId)
+
+  res.render('edit', { room })
+})
+
+app.post('/admin/rooms/edit/:id', function (req, res) {
+  var roomId = req.params.id
+
+  var room = _.find(rooms, r => r.id === roomId)
+  room.name = req.body.name
 
   res.redirect('/admin/rooms')
 })
