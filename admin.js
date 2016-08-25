05ccb6b33e994b5507753f141e8e4884b5798171
Thomas Swann <ts.swann@gmail.com>
2016-08-25 20:46:25 +0100

    Moved admin routes to module

---
+++ admin.js
@@ -0,0 +1,57 @@
+var rooms = require('./data/rooms.json')
+var uuid = require('node-uuid')
+var _ = require('lodash')
+var express = require('express')
+var router = express.Router()
+module.exports = router
+
+router.get('/admin/rooms', function (req, res) {
+  res.render('rooms', { title: 'Admin Rooms', rooms: rooms })
+})
+
+router.get('/admin/rooms/add', function (req, res) {
+  res.render('add')
+})
+
+router.post('/admin/rooms/add', function (req, res) {
+  var room = {
+    name: req.body.name,
+    id: uuid.v4()
+  }
+
+  rooms.push(room)
+
+  res.redirect('/admin/rooms')
+})
+
+router.get('/admin/rooms/delete/:id', function (req, res) {
+  var roomId = req.params.id
+
+  rooms = _.find(rooms, r => r.id !== roomId)
+
+  res.redirect('/admin/rooms')
+})
+
+router.get('/admin/rooms/edit/:id', function (req, res) {
+  var roomId = req.params.id
+
+  var room = _.find(rooms, r => r.id === roomId)
+  if (!room) {
+    res.sendStatus(404)
+  }
+
+  res.render('edit', { room })
+})
+
+router.post('/admin/rooms/edit/:id', function (req, res) {
+  var roomId = req.params.id
+
+  var room = _.find(rooms, r => r.id === roomId)
+  if (!room) {
+    res.sendStatus(404)
+  }
+
+  room.name = req.body.name
+
+  res.redirect('/admin/rooms')
+})
--- app.js
+++ app.js
@@ -1,69 +1,17 @@
 var express = require('express')
-var rooms = require('./data/rooms.json')
 var bodyParser = require('body-parser')
-var uuid = require('node-uuid')
-var _ = require('lodash')
 var app = express()
+var admin = require('./admin')
 
 app.set('view engine', 'jade')
 app.set('views', './views')
+app.use(admin)
 app.use(express.static('public'))
 app.use(express.static('node_modules/bootstrap/dist'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Home' })
-})
-
-app.get('/admin/rooms', function (req, res) {
-  res.render('rooms', { title: 'Admin Rooms', rooms: rooms })
-})
-
-app.get('/admin/rooms/add', function (req, res) {
-  res.render('add')
-})
-
-app.post('/admin/rooms/add', function (req, res) {
-  var room = {
-    name: req.body.name,
-    id: uuid.v4()
-  }
-
-  rooms.push(room)
-
-  res.redirect('/admin/rooms')
-})
-
-app.get('/admin/rooms/delete/:id', function (req, res) {
-  var roomId = req.params.id
-
-  rooms = _.find(rooms, r => r.id !== roomId)
-
-  res.redirect('/admin/rooms')
-})
-
-app.get('/admin/rooms/edit/:id', function (req, res) {
-  var roomId = req.params.id
-
-  var room = _.find(rooms, r => r.id === roomId)
-  if (!room) {
-    res.sendStatus(404)
-  }
-
-  res.render('edit', { room })
-})
-
-app.post('/admin/rooms/edit/:id', function (req, res) {
-  var roomId = req.params.id
-
-  var room = _.find(rooms, r => r.id === roomId)
-  if (!room) {
-    res.sendStatus(404)
-  }
-
-  room.name = req.body.name
-
-  res.redirect('/admin/rooms')
 })
 
 app.listen(3000, function () {
